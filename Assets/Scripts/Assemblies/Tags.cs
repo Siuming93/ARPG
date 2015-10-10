733c03d48209792b43151f6a3cc7775f2ea2ee40
SiumingPC <353302109@qq.com>
2015-10-10 20:32:23 +0800

    创建Tags类来配置tag

---
+++ Assets/Scripts/Assemblies/Tags.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Tags  
+{
+    public const string MainCamera = "MainCamera";
+    public const string Player = "Player";
+    public const string Enemy = "Enemy";
+}
