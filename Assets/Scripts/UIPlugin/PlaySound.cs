57f974c08048b721d5002eb7b4025405db16558f
SiuMing <2457944792@qq.com>
2015-10-10 15:01:55 +0800

    创建了PlaySound脚本来让button可以在点击的时候从给定的声音位置播放音效

---
+++ Assets/Scripts/UIPlugin/PlaySound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlaySound : MonoBehaviour,IPointerClickHandler
+{
+    public AudioClip clip;      //要播放的声音
+    public AudioSource source;  //播放声音的源头
+
+    /// <summary>
+    /// 鼠标点击之后,audioSource播放声音
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        source.PlayOneShot(clip);
+    }
+}
