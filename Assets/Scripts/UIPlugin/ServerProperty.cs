61194caddf308f238daf631873f9f8409210a8ef
SiumingPC <353302109@qq.com>
2015-10-12 00:06:05 +0800

    服务器button的属性,包括ip,名称,人数和点击后通过广播来调用startnumecontroller中的serverButtonClick函数

---
+++ Assets/Scripts/UIPlugin/ServerProperty.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ServerProperty : MonoBehaviour
+{
+    public Text text;
+    public int count = 100;
+
+    string ip = "127.0.0.1:8090";
+    string name = "一区 马达加斯加";
+
+    public void Set(string ip, string name)
+    {
+        this.ip = ip;
+        this.name = name;
+
+        text.text = name;
+    }
+
+    public void SelectButtonClick()
+    {
+        transform.root.SendMessage("OnServerButtonClick", this.gameObject);
+    }
+}
