bf32fdaa1aff30cde67589e85623730c297fa83e
SiuMing <2457944792@qq.com>
2015-10-12 16:55:40 +0800

    用于UI控件的移动动画

---
+++ Assets/Scripts/UIPlugin/UIMoveAuto.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+
+public class UIMoveAuto : MonoBehaviour
+{
+    public float Distance;
+    public float Duration;
+
+
+    public void Start()
+    {
+        //自动循环播放
+        Image image = this.GetComponent<Image>();
+        Tweener mover = image.rectTransform.DOMoveX(Distance, Duration);
+        mover.SetEase(Ease.Linear);
+        mover.SetLoops(int.MaxValue, LoopType.Restart);
+    }
+}
