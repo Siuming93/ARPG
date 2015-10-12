1fddeb8e64de250051dc8947ce5429115d5694ba
SiuMing <2457944792@qq.com>
2015-10-12 15:41:38 +0800

    将火爆与流畅的背景图片更改挪到了这里

--- Assets/Scripts/UIPlugin/ServerProperty.cs
+++ Assets/Scripts/UIPlugin/ServerProperty.cs
@@ -5,17 +5,28 @@ using System.Collections;
 public class ServerProperty : MonoBehaviour
 {
     public Text text;
+    public Image image;   
+    public Sprite ServerItemSpriteRed;
+    public Sprite ServerItemSpriteGreen;
+
+    [HideInInspector]
+    public string ip = "127.0.0.1:8090";
+    [HideInInspector]
+    public string name = "一区 马达加斯加";
+    [HideInInspector]
     public int count = 100;
 
-    string ip = "127.0.0.1:8090";
-    string name = "一区 马达加斯加";
-
-    public void Set(string ip, string name)
+    public void Set(string ip, string name, int count)
     {
         this.ip = ip;
         this.name = name;
+        this.count = count;
 
         text.text = name;
+        if (count > 50)
+            image.sprite = ServerItemSpriteRed;
+        else
+            image.sprite = ServerItemSpriteGreen;
     }
 
     public void SelectButtonClick()
