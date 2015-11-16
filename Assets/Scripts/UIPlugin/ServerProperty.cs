b2b4b9e693654f023683b5a9729778b42a6abfc3
Siuming <2457944792@qq.com>
2015-11-16 10:19:20 +0800

    更改了格式

--- Assets/Scripts/UIPlugin/ServerProperty.cs
+++ Assets/Scripts/UIPlugin/ServerProperty.cs
@@ -4,29 +4,23 @@ using System.Collections;
 
 public class ServerProperty : MonoBehaviour
 {
-    public Text text;
-    public Image image;   
+    public Text Text;
+    public Image Image;
     public Sprite ServerItemSpriteRed;
     public Sprite ServerItemSpriteGreen;
 
-    [HideInInspector]
-    public string ip = "127.0.0.1:8090";
-    [HideInInspector]
-    public string name = "一区 马达加斯加";
-    [HideInInspector]
-    public int count = 100;
+    [HideInInspector] public string Ip = "127.0.0.1:8090";
+    [HideInInspector] public string Name = "一区 马达加斯加";
+    [HideInInspector] public int Count = 100;
 
     public void Set(string ip, string name, int count)
     {
-        this.ip = ip;
-        this.name = name;
-        this.count = count;
+        this.Ip = ip;
+        this.Name = name;
+        this.Count = count;
 
-        text.text = name;
-        if (count > 50)
-            image.sprite = ServerItemSpriteRed;
-        else
-            image.sprite = ServerItemSpriteGreen;
+        Text.text = name;
+        Image.sprite = count > 50 ? ServerItemSpriteRed : ServerItemSpriteGreen;
     }
 
     public void SelectButtonClick()
