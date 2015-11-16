044d456b846f7f3ebb2fd9268e0b5721ddf7c1da
Siuming <2457944792@qq.com>
2015-11-16 10:20:59 +0800

    增加了message模块

---
+++ Assets/Scripts/UIPlugin/MessageManger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageManger : MonoBehaviour
+{
+    public Text MessageText;
+
+    public static MessageManger Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SetMessage(string message, float timer = 2f)
+    {
+        MessageText.text = message;
+        StartCoroutine(ClearMessage(timer));
+    }
+
+    private IEnumerator ClearMessage(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+
+        MessageText.text = "";
+    }
+}
