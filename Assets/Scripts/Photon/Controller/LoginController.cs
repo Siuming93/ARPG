c8fb35d5c7bbbaf40e4b578f0fa94efb7f6da73e
Siuming <2457944792@qq.com>
2015-11-18 17:48:38 +0800

    脚本一些格式和命名空间引用的改动,不涉及功能

--- Assets/Scripts/Photon/Controller/LoginController.cs
+++ Assets/Scripts/Photon/Controller/LoginController.cs
@@ -3,16 +3,19 @@ using ARPGCommon;
 using ARPGCommon.Model;
 using ExitGames.Client.Photon;
 using LitJson;
-using UnityEditor.VersionControl;
-using UnityEngine.UI;
 
 public class LoginController : ControllerBase
 {
+    protected override OperationCode OpCode
+    {
+        get { return OperationCode.Login; }
+    }
+
     public void LoginRequest(string username, string password)
     {
         //1.初始化参数
         var json = JsonMapper.ToJson(new User {Username = username, Password = password});
-        var parameters = new Dictionary<byte, object> {{(byte) ParameterCode.UserCheckInfo, json.ToString()}};
+        var parameters = new Dictionary<byte, object> {{(byte) ParameterCode.UserCheckInfo, json}};
 
         //2.发送登录请求与帐号密码
         PhotonEngine.Instance.SendOperationRequest(OperationCode.Login, parameters);
@@ -24,22 +27,14 @@ public class LoginController : ControllerBase
         switch (response.ReturnCode)
         {
             case (short) ReturnCode.Success:
-                //1.加载角色信息
-                //TODO
-                //2.返回开始
+                //返回开始
                 StartMenuController.Instance.OnLoginSuccess();
-
                 break;
 
             case (short) ReturnCode.Fail:
-                //1.登录失败,提示
+                //登录失败,提示
                 MessageManger.Instance.SetMessage(response.DebugMessage);
                 break;
         }
-    }
-
-    protected override OperationCode OpCode
-    {
-        get { return OperationCode.Login; }
     }
 }
