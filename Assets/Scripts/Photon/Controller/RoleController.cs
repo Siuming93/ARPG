64e4614284a4a1c5ec54c9380d6be33d90688ef6
Siuming <2457944792@qq.com>
2015-11-18 17:49:52 +0800

    更改了接受响应后的方法,之前写反了

--- Assets/Scripts/Photon/Controller/RoleController.cs
+++ Assets/Scripts/Photon/Controller/RoleController.cs
@@ -1,40 +1,41 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using ARPGCommon;
 using ARPGCommon.Model;
+using ExitGames.Client.Photon;
+using LitJson;
 
 public class RoleController : ControllerBase
 {
-    protected override ARPGCommon.OperationCode OpCode
+    protected override OperationCode OpCode
     {
         get { return OperationCode.Role; }
     }
 
     public void GetRole()
     {
-        var parameter = new Dictionary<byte, object> { { (byte)ParameterCode.SubCode, SubCode.GetRole } };
+        var parameter = new Dictionary<byte, object> {{(byte) ParameterCode.SubCode, SubCode.GetRole}};
 
         PhotonEngine.Instance.SendOperationRequest(OpCode, parameter);
     }
 
     public void AddRole()
     {
-        var parameter = new Dictionary<byte, object> { { (byte)ParameterCode.SubCode, SubCode.AddRole } };
+        var parameter = new Dictionary<byte, object> {{(byte) ParameterCode.SubCode, SubCode.AddRole}};
 
         PhotonEngine.Instance.SendOperationRequest(OpCode, parameter);
     }
 
-    public override void OnOperationResponse(ExitGames.Client.Photon.OperationResponse response)
+    public override void OnOperationResponse(OperationResponse response)
     {
         var subCode = ParameterTool.GetParameter<SubCode>(response.Parameters, ParameterCode.SubCode);
         switch (subCode)
         {
-            case SubCode.AddRole:
-                var roleListDb = ParameterTool.GetParameter<List<Role>>(response.Parameters, ParameterCode.RoleList);
+            case SubCode.GetRole:
+                var json = ParameterTool.GetParameter<object>(response.Parameters, ParameterCode.RoleList);
+                var roleListDb = JsonMapper.ToObject<List<Role>>(json.ToString());
                 OnGetRole(roleListDb);
                 break;
-            case SubCode.GetRole:
+            case SubCode.AddRole:
                 var role = ParameterTool.GetParameter<Role>(response.Parameters, ParameterCode.Role);
                 OnAddRole(role);
                 break;
