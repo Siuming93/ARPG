4e6b3099a198a7ea3a9b0512a1ca3909ac91fb45
Siuming <2457944792@qq.com>
2015-11-18 17:47:17 +0800

    增加了进入游戏后选择角色的功能

--- Assets/Scripts/UIPlugin/StartMenuController.cs
+++ Assets/Scripts/UIPlugin/StartMenuController.cs
@@ -1,49 +1,57 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using ARPGCommon.Model;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class StartMenuController : MonoBehaviour
 {
     public static StartMenuController Instance { get; private set; }
 
+    //UI缩放动画
     public UIScale StartScale;
     public UIScale LoginScale;
     public UIScale RegisterScale;
     public UIScale ServerScale;
-
+    //UI移动动画
     public UIMove CharcterSelectMove;
     public UIMove StartMove;
     public UIMove CharcterChnageMove;
-
+    //PhotonEngine的控制器
     public LoginController LoginController;
     public RegisterController RegisterController;
-
+    public RoleController RoleController;
+    //登录输入框
     public InputField UsernameInputField;
     public InputField PasswordInputField;
-
+    //注册输入框
     public InputField RegisterUsernameInputField;
     public InputField RegisterPasswordInputField;
     public InputField RegisterPasswordConfirmInputField;
-
+    //角色信息?
     public Text Name;
     public Text Level;
     public Text NewName;
-
-    public Transform ServerGridVerticalCell1;
-    public Transform ServerGridVerticalCell2;
+    //创建角色时名称输入框
+    public InputField CharcterNameInputField;
+    //不知道了..囧
     public Transform CharcterSelectedParent; //当前选中的角色模型的父GameObject
+    //已选择的服务器Button
+    public GameObject ServerSelectedButton; //
+    //开始面板的两个按钮
+    public Text ServerShowButtonText; //显示服务器的按钮
+    public Text UserNameButtonText; //显示用户名称的按钮
 
-    public GameObject ServerSelectedButton; //已选择的服务器Button
-    public GameObject ServerShowButton; //显示服务器的按钮
-    public GameObject UserNameButton; //显示用户名称的按钮
+    /// <summary>
+    /// 从创建角色返回选择角色的按钮
+    /// </summary>
+    public GameObject BackToSelectCharcterGameObject;
 
+    //当前可用的角色模型
     public GameObject[] CharcterArray;
-
-    //Prefabs
-    public GameObject ServerItem;
-
-    private ServerProperty _curServerInfo; //已选择的服务器
+    //私有属性
+    private ServerUiProperty _curServerUiInfo; //已选择的服务器
     private UIScale _choiceCharcterUiScale;
-    private int _index; //创建角色时所选中的角色的索引
+    private int _index = -1; //创建角色时所选中的角色的索引
 
     private void Awake()
     {
@@ -53,6 +61,51 @@ public class StartMenuController : MonoBehaviour
 
     private void Start()
     {
+        RoleController.OnAddRole += OnAddRole;
+        RoleController.OnGetRole += OnGetRole;
+    }
+
+    private void Destory()
+    {
+        RoleController.OnAddRole -= OnAddRole;
+        RoleController.OnGetRole -= OnGetRole;
+    }
+
+    public void OnAddRole(Role role)
+    {
+    }
+
+    public void OnGetRole(List<Role> roles)
+    {
+        if (roles != null && roles.Count > 0)
+        {
+            UpdateCharcterSelected(roles[0]);
+            StartMove.MoveOut(Direction.Right);
+            CharcterSelectMove.MoveIn(Direction.Left);
+        }
+        else
+        {
+            BackToSelectCharcterGameObject.SetActive(false);
+            StartMove.MoveOut(Direction.Right);
+            CharcterChnageMove.MoveIn(Direction.Left);
+        }
+    }
+
+    private void UpdateCharcterSelected(Role role)
+    {
+        //1.将之前的模型销毁
+        Destroy(CharcterSelectedParent.GetComponentInChildren<Animation>().gameObject);
+
+        //2.创建新的角色模型
+        var index = role.Isman ? 0 : 1;
+        var newCharcter =
+            Instantiate(CharcterArray[index], CharcterSelectedParent.position, CharcterSelectedParent.rotation) as
+                GameObject;
+        newCharcter.transform.parent = CharcterSelectedParent;
+
+        //.3更新角色信息
+        Name.text = role.Name;
+        Level.text = "Lv" + role.Level;
     }
 
     /// <summary>
@@ -60,21 +113,24 @@ public class StartMenuController : MonoBehaviour
     /// </summary>
     public void OnCharcterCreateButtonClick()
     {
-        //1.连接服务器,验证昵称是否可用
+        //1.本地验证
+        if (_index == -1)
+        {
+            MessageManger.Instance.SetMessage("请选择一个角色!");
+            return;
+        }
+        if (CharcterNameInputField.text == "" || CharcterNameInputField.text.Length > 20)
+        {
+            MessageManger.Instance.SetMessage("昵称不合法!");
+            return;
+        }
+        //2.连接服务器,验证昵称是否可用
         //TODO
         //2.选择了角色
         //TODO
 
         //3.将选中的角色替换掉
-        Destroy(CharcterSelectedParent.GetComponentInChildren<Animation>().gameObject);
-        var newCharcter =
-            Instantiate(CharcterArray[_index], CharcterSelectedParent.position,
-                CharcterSelectedParent.rotation) as GameObject;
-        newCharcter.transform.parent = CharcterSelectedParent;
-
-        //4.更新角色信息
-        Name.text = NewName.text;
-        Level.text = "Lv.1";
+        UpdateCharcterSelected(new Role {Name = "0", Level = 0, Isman = _index == 0});
 
         //5.跳转回去
         OnChangeCharcterBackButtonClick();
@@ -107,19 +163,24 @@ public class StartMenuController : MonoBehaviour
     }
 
     /// <summary>
-    /// 选中的角色要放大显示
+    /// 获得选中的模型id
     /// </summary>
     /// <param name="selectedUi"></param>
-    public void OnChoiceCharcterClick(GameObject selectedUi)
+    public void OnChoiceCharcterClick(int id)
+    {
+        _index = id;
+    }
+
+    /// <summary>
+    /// 选中的角色要放大显示
+    /// </summary>
+    /// <param name="selectedUiScale"></param>
+    public void OnChoiceCharcterClick(UIScale selectedUiScale)
     {
         if (_choiceCharcterUiScale != null)
         {
             _choiceCharcterUiScale.Scale(new Vector3(1f, 1f, 1f), 0.5f);
         }
-
-        _index = selectedUi.name.Contains("Boy") ? 0 : 1;
-
-        var selectedUiScale = selectedUi.GetComponent<UIScale>();
         selectedUiScale.Scale(new Vector3(1.4f, 1.4f, 1.4f), 0.5f);
         _choiceCharcterUiScale = selectedUiScale;
     }
@@ -140,17 +201,19 @@ public class StartMenuController : MonoBehaviour
     public void OnEnterGameClick()
     {
         //1.连接服务器，验证用户名和服务器
-        //TODO
+
 
         //2.进入角色选择页面
+        //3.加载角色信息
+        RoleController.GetRole();
 
         //设定移动的目标
         //float distance = StartMoveOut.transform.position.x - CharcterMoveIn.transform.position.x;
-        CharcterSelectMove.Target = StartMove.transform.position.x;
-        StartMove.Target = -CharcterSelectMove.transform.position.x;
+        //CharcterSelectMove.Target = StartMove.transform.position.x;
+        //StartMove.Target = -CharcterSelectMove.transform.position.x;
 
-        StartMove.MoveAndSetActive(false);
-        CharcterSelectMove.MoveAndSetActive(true);
+        //StartMove.MoveAndSetActive(false);
+        //CharcterSelectMove.MoveAndSetActive(true);
     }
 
     /// <summary>
@@ -171,25 +234,18 @@ public class StartMenuController : MonoBehaviour
     /// </summary>
     public void OnLoginSuccess()
     {
+        //1.设定用户名称
+        UserNameButtonText.text = UsernameInputField.text;
+        //2.跳转到选择服务器
         LoginScale.SetActive(false);
         StartScale.SetActive(true);
-        //设定用户名称
-        UserNameButton.GetComponentInChildren<Text>().text = UsernameInputField.text;
+        //3.选择服务器
+        //TODO
     }
 
-    public void OnLoginButtonClick(Text text, Text text2)
+    public void OnSelectServer()
     {
-        //1.验证用户名和密码
-        //LoginController.LoginRequest();
-
-        //2.验证成功
-        //返回开始界面
-        LoginScale.SetActive(false);
-        StartScale.SetActive(true);
-        //设定用户名称
-        UserNameButton.GetComponentInChildren<Text>().text = text.text;
-
-        //3.验证失败
+        //1.加载角色信息
         //TODO
     }
 
@@ -278,16 +334,18 @@ public class StartMenuController : MonoBehaviour
     /// <summary>
     /// 服务器选择按钮点击事件
     /// </summary>
-    public void OnServerButtonClick(GameObject sender)
+    public void OnServerButtonClick(ServerUiProperty serverUi)
     {
         //1.记录
-        _curServerInfo = sender.GetComponent<ServerProperty>();
-        var curImage = sender.GetComponent<Image>();
+        _curServerUiInfo = serverUi;
 
         //2.设置已选择的服务器
         ServerSelectedButton.SetActive(true);
-        var serverButtonSelectedSp = ServerSelectedButton.GetComponent<ServerProperty>();
-        serverButtonSelectedSp.Set(_curServerInfo.Ip, _curServerInfo.Name, _curServerInfo.Count);
+        var serverButtonSelectedSp = ServerSelectedButton.GetComponent<ServerUiProperty>();
+        serverButtonSelectedSp.Set(_curServerUiInfo.Ip, _curServerUiInfo.Name, _curServerUiInfo.Count);
+
+        //3.设置开始面板上的服务器名称
+        ServerShowButtonText.text = _curServerUiInfo.Name;
     }
 
     /// <summary>
@@ -298,8 +356,5 @@ public class StartMenuController : MonoBehaviour
         //1.跳转
         ServerScale.SetActive(false);
         StartScale.SetActive(true);
-
-        //2.设置开始面板上的服务器名称
-        ServerShowButton.GetComponentInChildren<Text>().text = _curServerInfo.Name;
     }
 }
