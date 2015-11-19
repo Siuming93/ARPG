6e0b2a09c783432332f358bf589e1e753f731c2b
SiumingPC <353302109@qq.com>
2015-11-19 20:34:28 +0800

    基本完成了角色的创建,获取和选择

--- Assets/Scripts/Photon/Controller/RoleController.cs
+++ Assets/Scripts/Photon/Controller/RoleController.cs
@@ -18,30 +18,60 @@ public class RoleController : ControllerBase
         PhotonEngine.Instance.SendOperationRequest(OpCode, parameter);
     }
 
-    public void AddRole()
+    public void AddRole(Role role)
     {
-        var parameter = new Dictionary<byte, object> {{(byte) ParameterCode.SubCode, SubCode.AddRole}};
+        var parameter = new Dictionary<byte, object>
+        {
+            {(byte) ParameterCode.SubCode, SubCode.AddRole},
+            {(byte) ParameterCode.Role, JsonMapper.ToJson(role)}
+        };
+        PhotonEngine.Instance.SendOperationRequest(OpCode, parameter);
+    }
 
+    /// <summary>
+    /// 选择进入游戏之后的角色
+    /// </summary>
+    /// <param name="role"></param>
+    public void SelectRole(Role role)
+    {
+        var parameter = new Dictionary<byte, object>
+        {
+            {(byte) ParameterCode.SubCode, SubCode.SelectRole},
+            {(byte) ParameterCode.Role, JsonMapper.ToJson(role)}
+        };
         PhotonEngine.Instance.SendOperationRequest(OpCode, parameter);
     }
 
     public override void OnOperationResponse(OperationResponse response)
     {
-        var subCode = ParameterTool.GetParameter<SubCode>(response.Parameters, ParameterCode.SubCode);
+        var subCode = ParameterTool.GetParameter<SubCode>(response.Parameters, ParameterCode.SubCode, false);
         switch (subCode)
         {
             case SubCode.GetRole:
-                var json = ParameterTool.GetParameter<object>(response.Parameters, ParameterCode.RoleList);
-                var roleListDb = JsonMapper.ToObject<List<Role>>(json.ToString());
+                var roleListDb = ParameterTool.GetParameter<List<Role>>(response.Parameters, ParameterCode.RoleList);
                 OnGetRole(roleListDb);
                 break;
             case SubCode.AddRole:
-                var role = ParameterTool.GetParameter<Role>(response.Parameters, ParameterCode.Role);
-                OnAddRole(role);
+                switch (response.ReturnCode)
+                {
+                    case (short) ReturnCode.Success:
+                        var role = ParameterTool.GetParameter<Role>(response.Parameters, ParameterCode.Role);
+                        OnAddRole(role);
+                        break;
+                    case (short) ReturnCode.Fail:
+                        MessageManger.Instance.SetMessage(response.DebugMessage);
+                        break;
+                }
+
+                break;
+            case SubCode.SelectRole:
+                if (OnSelectRole != null)
+                    OnSelectRole();
                 break;
         }
     }
 
     public event OnGetRoleEvent OnGetRole;
     public event OnAddRoleEvent OnAddRole;
+    public event OnSelectRoleEvent OnSelectRole;
 }
