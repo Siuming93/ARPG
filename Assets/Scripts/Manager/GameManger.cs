984e379af1fafe7c7683df3e296d4be7aa94c664
SiumingPC <353302109@qq.com>
2015-11-19 23:04:15 +0800

    增加了GameManager

---
+++ Assets/Scripts/Manager/GameManger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using ARPGCommon.Model;
+
+public class GameManger: MonoBehaviour {
+    //当前角色
+    private Role _curRole;
+    void Awake()
+    {
+        _curRole = PhotonEngine.Instance.CurRole;
+        //1.根据当前的Role加载模型;
+        //TODO
+    }
+}
