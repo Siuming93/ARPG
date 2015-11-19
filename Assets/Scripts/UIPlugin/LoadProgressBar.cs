f59e832cf232925650b76a4c71a194d8be09e0d8
SiumingPC <353302109@qq.com>
2015-11-19 20:34:49 +0800

    增加了场景加载进度显示

---
+++ Assets/Scripts/UIPlugin/LoadProgressBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LoadProgressBar : MonoBehaviour
+{
+    public Slider ProgressSlider;
+    public Text SliderText;
+
+    private AsyncOperation _operation;
+
+    public void Update()
+    {
+        ProgressSlider.value = _operation.progress;
+        SliderText.text = (int) (_operation.progress*100f) + "%";
+    }
+
+    public void Show(AsyncOperation operation)
+    {
+        gameObject.SetActive(true);
+        this._operation = operation;
+    }
+}
