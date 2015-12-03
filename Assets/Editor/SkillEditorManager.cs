b5964399b28dbe6d329c6bd2453dadb2c59328df
Siuming <2457944792@qq.com>
2015-12-03 17:44:40 +0800

    增加了技能编辑器的窗口类和技能编辑器的管理类

---
+++ Assets/Editor/SkillEditorManager.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Assets.Scripts.View.Skill;
+using Assets.Scripts.View.Skill.Action;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+    public class SkillEditorManager
+    {
+        public static TreeViewItem SelectedItem;
+        public static string Path = "Assets/Resources/SkillData";
+
+        public static void Handler(object sender, System.EventArgs args)
+        {
+            Debug.Log(string.Format("{0} detected: {1}", args.GetType().Name, (sender as TreeViewItem).Header));
+        }
+
+        public static void CreateSkill(int id)
+        {
+            var skill = ScriptableObject.CreateInstance<Skill>();
+            skill.Id = id;
+
+            var newItem = SelectedItem.AddItem(id.ToString());
+            var path = Path + "/" + skill.Id + "/" + "Skill" + skill.Id + ".asset";
+
+            newItem.DataContext = new ItemData()
+            {
+                SelectedType = SelectedType.Skill,
+                Path = path,
+                Name = skill.Id.ToString(),
+                Data = skill
+            };
+            newItem.IsSelected = true;
+            AddEvents(newItem);
+
+            AssetDatabase.CreateFolder(Path, skill.Id.ToString());
+            AssetDatabase.CreateAsset(skill, path);
+        }
+
+        public static void CreateActionAsset(TreeViewItem parentItem, ActionBase action)
+        {
+            var newItem = parentItem.AddItem(action.GetType().ToString());
+            var data = parentItem.DataContext as ItemData;
+            var path = Path + "/" + data.Name;
+
+            newItem.DataContext = new ItemData() {SelectedType = SelectedType.Skill, Path = path};
+            newItem.IsSelected = true;
+            AddEvents(newItem);
+
+            AssetDatabase.CreateFolder(path, "Actions");
+            AssetDatabase.CreateAsset(action, path + "/Actions/" + action.GetType() + ".asset");
+        }
+
+        public static void CreateAction(Type actionType)
+        {
+            var action = ScriptableObject.CreateInstance<AnimationAction>();
+            CreateActionAsset(SelectedItem, action);
+        }
+
+        public static void SelectedHanlder(object sender, System.EventArgs args)
+        {
+            var data = (ItemData) (sender as TreeViewItem).DataContext;
+            SelectedItem = sender as TreeViewItem;
+
+            switch (data.SelectedType)
+            {
+                case SelectedType.Root:
+                    break;
+                case SelectedType.Skill:
+                    //选择操作
+                    break;
+                case SelectedType.Action:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private static void AddHandlerEvent(out System.EventHandler handler)
+        {
+            handler = Handler;
+        }
+
+        private static void AddEvents(TreeViewItem item)
+        {
+            AddHandlerEvent(out item.Click);
+            AddHandlerEvent(out item.Checked);
+            AddHandlerEvent(out item.Unchecked);
+            AddHandlerEvent(out item.Unselected);
+            item.Selected = SelectedHanlder;
+        }
+
+        public static void InitTree(TreeViewControl item)
+        {
+            item.Width = 600;
+            item.Height = 500;
+            item.Header = "Skill List";
+            AddEvents(item.RootItem);
+            var item1 = item.RootItem.AddItem("skill1");
+
+            AddEvents(item1);
+            item.DataContext = new ItemData() {SelectedType = SelectedType.Root};
+            item1.DataContext = new ItemData() {SelectedType = SelectedType.Skill};
+        }
+    }
+
+    internal enum SelectedType : byte
+    {
+        Root,
+        Skill,
+        Action
+    }
+
+    internal class ItemData
+    {
+        public SelectedType SelectedType;
+        public string Path;
+        public string Name;
+        public object Data;
+
+        public Type Type
+        {
+            get { return Data.GetType(); }
+        }
+    }
+}
---
+++ Assets/Editor/SkillEditorWindow.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Assets.Scripts.View.Skill.Action;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+    public class SkillEditorWindow : EditorWindow
+    {
+        [MenuItem("Tools/SkillEditorWindow")]
+        public static void ShowSkillEditor()
+        {
+            CreateTreeView();
+            RefreashPanel();
+        }
+
+        public static SkillEditorWindow _instance;
+
+        public static SkillEditorWindow GetPanel()
+        {
+            if (_instance == null)
+            {
+                _instance = EditorWindow.GetWindow<SkillEditorWindow>(false, "SkillEditorWindow", false);
+            }
+
+            return _instance;
+        }
+
+        public static void RefreashPanel()
+        {
+            var panel = GetPanel();
+            panel.Repaint();
+        }
+
+        private static TreeViewControl _treeViewControl;
+
+        public static void CreateTreeView()
+        {
+            _treeViewControl = TreeViewInspector.AddTreeView();
+            _treeViewControl.DisplayInInspector = false;
+            _treeViewControl.DisplayOnScene = false;
+
+            SkillEditorManager.InitTree(_treeViewControl);
+        }
+
+        private void OnEnable()
+        {
+            wantsMouseMove = true;
+        }
+
+        private void OnGUI()
+        {
+            if (_treeViewControl == null)
+            {
+                return;
+            }
+
+            wantsMouseMove = true;
+            if (Event.current != null && Event.current.type == EventType.MouseMove)
+            {
+                Repaint();
+            }
+            _treeViewControl.DisplayTreeView(TreeViewControl.DisplayTypes.USE_SCROLL_VIEW);
+
+            if (SkillEditorManager.SelectedItem == null)
+                return;
+
+            //根据选择的不同标签,显示不同的button
+            var data = SkillEditorManager.SelectedItem.DataContext as ItemData;
+            switch (data.SelectedType)
+            {
+                case SelectedType.Root:
+                    if (GUILayout.Button("AddSkill"))
+                    {
+                        SkillEditorManager.CreateSkill(0);
+                    }
+                    break;
+                case SelectedType.Skill:
+
+                    GUILayout.BeginHorizontal();
+                    //1.获得当前的种类
+                    GUILayout.Label("Choice ActionType");
+                    GUIContent[] content = new GUIContent[]
+                    {new GUIContent() {text = "ff"}, new GUIContent() {text = "ff"}, new GUIContent() {text = "ff"},};
+                    GUILayout.SelectionGrid(0, content, 1);
+                    GUILayout.EndHorizontal();
+                    if (GUILayout.Button("AddAction"))
+                    {
+                        SkillEditorManager.CreateAction(typeof (AnimationAction));
+                    }
+                    break;
+                case SelectedType.Action:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
